7bde7d2a03331417ab8ba42f03ab4d72a4f3e270
Moc111 <vuthang28801@gmail.com>
2022-09-05 18:34:57 +0700

    Day la Source dau tien

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Khai bao
+ * xin chao
+ * 1 
+ * 2
+ * 3
+ * 
+ */
+
+
+alert('Xin chào');
+
+var myString = 'Vu Huu Thang';
+
+console.log(myString.length);
+
+console.log(myString.indexOf('T'));
